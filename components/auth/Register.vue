2e49a39775360e6eb51b346ae16a59c66768cb17
Guillaume Duhan <myhappyagency@gmail.com>
2020-09-30 19:39:45 +0200

    Dashboard header

--- components/auth/Register.vue
+++ components/auth/Register.vue
@@ -1,9 +1,42 @@
 <template>
-  <div class="Register" />
+  <div class="Register">
+    <div>
+      <div>
+        <label for="user-mail">E-mail</label>
+        <b-input id="user-mail" v-model="email" type="email" />
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <b-input id="password" v-model="password" type="password" />
+      </div>
+      <div>
+        <label for="confirm_password">Confirm password</label>
+        <b-input id="confirm_password" v-model="confirm_password" type="password" />
+      </div>
+    </div>
+    <b-button variant="primary" @click="register">Register</b-button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Register",
+  data() {
+    return {
+      email: undefined,
+      password: undefined,
+      confirm_password: undefined
+    }
+  },
+  methods: {
+    mailValidator() {
+      /* eslint-disable */
+      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return re.test(String(this.email).toLowerCase());
+    },
+    async register() {
+
+    },
+  },
 }
 </script>
---
+++ components/nav/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="Header d-flex align-items-center justify-content-between bg-dark text-white px-4 py-2">
+    <div>Dashboard</div>
+    <div>
+      {{ $auth.user.email }}
+      <b-button variant="danger" @click="$auth.logout()">Logout</b-button>
+    </div> 
+  </div>
+</template>
