977f9a6353d6c9ee658d4ee1521be14c1a86263b
Guillaume Duhan <myhappyagency@gmail.com>
2020-10-30 23:05:24 +0000

    add eslint.rc file

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+  extends: ["eslint:recommended", "plugin:vue/recommended"],
+  // required to lint *.vue files
+  plugins: ["vue"],
+  // add your custom rules here
+  rules: {
+    "indent": ["error", 2, { "SwitchCase": 1 }],
+    semi: [2, "never"],
+    "no-console": "off",
+    "vue/max-attributes-per-line": "off",
+    "vue/singleline-html-element-content-newline": "off",
+    "vue/multiline-html-element-content-newline": "off",
+  },
+}
