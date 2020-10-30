c4066de36865282ae739a31cbd75da1cd8c2a32f
Guillaume Duhan <myhappyagency@gmail.com>
2020-10-30 23:05:43 +0000

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,27 +2,10 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">
+      <h1 class="title text-primary">
         nuxt-bootstrap-boilerplate
       </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <div></div>
     </div>
   </div>
 </template>
